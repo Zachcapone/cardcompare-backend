cefae21d3230345760ae436b9403f92f6a3a46d5
Zachary Capone <105023055+Zachcapone@users.noreply.github.com>
2025-05-24 16:49:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,7 @@ def get_transactions():
     return jsonify(response.to_dict())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
